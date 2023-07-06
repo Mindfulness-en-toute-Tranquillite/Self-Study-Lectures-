d417b31c42441bc5bffb2cfe527985c2b4df1526
Jay <tvghoy@gmail.com>
2023-07-06 18:49:03 +0900

    Section2. Express 란? 2)  Server에서 파일 전송하기 (1)

--- my_app/index.js
+++ my_app/index.js
@@ -3,5 +3,18 @@ import express from 'express';
 const app = express();
 
 // middleware
+// main page GET
+app.get('/', (req, res) => {
+    // res.send('Main page GET request');
+    // res.send('<h1>Hello Main Page</h1>');
+    res.send(`
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <h4>H4</h4>
+    `)
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running');
+});
