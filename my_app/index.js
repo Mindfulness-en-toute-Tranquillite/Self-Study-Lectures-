4233ed31c200967ad333f5be3a6d8b2fd79bb72a
Jay <tvghoy@gmail.com>
2023-07-06 18:38:44 +0900

    Section2. Express 란? 1) Express로 server 구축하기 - Express 란?

--- my_app/index.js
+++ my_app/index.js
@@ -1,11 +1,7 @@
-import { createServer } from 'http' ;
+import express from 'express';
 
-const server = createServer((req, res) => {
-    res.writeHead(200, { 'Constent-Type': 'text/plain' });
-    res.write('Hello node.js');
-    res.end();
-});
+const app = express();
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
-}); 
+// middleware
+
+app.listen(3000);
