037dedf17771fb6c6f6cf757c612b3d175fadbe9
Jay <tvghoy@gmail.com>
2023-07-10 19:00:49 +0900

    Section4. MongoDB로 CRUD 구현하기 4) MongoDB 연결하기 - MongoDB 세팅하기

--- my_app/index.js
+++ my_app/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import nunjucks from 'nunjucks';
 import fs from 'fs';
+import mongoose from 'mongoose'
 
 const __dirname = path.resolve();
 
@@ -24,6 +25,26 @@ nunjucks.configure('views', {
     watch: true, // html 파일이 수정될 경우, 다시 반영 후 렌더링
     express: app
 })
+
+// mongoose connect
+mongoose
+    .connect('mongodb://127.0.0.1:27017')
+    .then(() => console.log('DB 연결 성공'))
+    .catch(e => console.error(e));
+
+// mongoose set
+const { Schema } = mongoose;
+
+const WritingSchema = new Schema({
+    title: String,
+    contents: String,
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const Writing = mongoose.model('Writing', WritingSchema);
 
 // middleware
 // main page GET
