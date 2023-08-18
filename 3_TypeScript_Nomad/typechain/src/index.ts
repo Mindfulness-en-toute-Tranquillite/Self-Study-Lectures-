fef871244dfdf843912f668615f09b0d051386d3
Jay <tvghoy@gmail.com>
2023-08-18 11:27:50 +0900

    #5.5 Blocks 강의 파일 및 코드

--- 3_TypeScript_Nomad/typechain/src/index.ts
+++ 3_TypeScript_Nomad/typechain/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hi")
+// import * as crypto from "crypto"
+import crypto from "crypto"; //tsconfig.json 에 "esModuleInterop": true 추가 및 "allowJs": true, 삭제. 이렇게 import 가능.
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape{
+    public hash: string;
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data:string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
